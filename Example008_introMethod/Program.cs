2df147fa97e1f8459f7ccd45c96bf6796f00b4e5
Andrei Shvetsov <shved963@gmail.com>
2022-11-30 15:06:06 +0300

    Project009

---
+++ Example008_introMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int a1 = 14;
+int b1 = 42;
+int c1 = 21;
+int a2 = 14;
+int b2 = 31;
+int c2 = 25;
+int a3 = 61;
+int b3 = 27;
+int c3 = 75;
+
+int max = a1;
---
+++ Example009_methofind/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int[] array = {10,18,22,33,14,15,14,16,17,18};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
