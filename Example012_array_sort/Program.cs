dac7e760bace73f41a46c8c29b5d56aeff8a8f66
Andrei Shvetsov <shved963@gmail.com>
2022-12-07 22:19:51 +0300

    ><

--- Example012_array_sort/Program.cs
+++ Example012_array_sort/Program.cs
@@ -20,7 +20,7 @@ void SelectionSort(int[] array)
         
         for (int j = i+1; j < array.Length; j++)
         {
-            if(array[j] < array[minPosition])
+            if(array[j] > array[minPosition])
             minPosition = j;
         }
         
